609c6eaf498dea277033b09fdebb3ed1571beabe
AvaPunzalan <avapunzalan2003@gmail.com>
2023-10-24 14:18:44 +0800

    Update
    Updated by Ava Punzalan

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,20 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     private Animation thisAnimation;
 
+    public Rigidbody rb;
+    public float jumpSpeed;
+
     void Start()
     {
         thisAnimation = GetComponent<Animation>();
         thisAnimation["Flap_Legacy"].speed = 3;
+
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
             thisAnimation.Play();
+            rb.AddForce(transform.up * jumpSpeed);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            SceneManager.LoadScene("GameOver");
+        }
     }
 }
